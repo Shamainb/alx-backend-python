30eda7fea8db07279e0ee349a44ff54bbf868023
Shamainb <shamainbhengesa60@gmail.com>
2024-07-27 15:11:29 +0200

    updating

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,12 @@ import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+"""
+Import wait_random from the previous
+python file that you’ve written and write
+Parameters: n,  max_delay, wait_random
+Returns: the list of all the delays (float values)
+"""
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
